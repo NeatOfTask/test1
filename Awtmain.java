f9b8bb5e0a2865c461f66b936dafbd8a6543f2ad
NeatOfTask <1towgje@gmail.com>
2016-07-07 02:00:44 +0900

    Add files via upload

---
+++ Awtmain.java
@@ -0,0 +1,9 @@
+package exm;
+public class Awtmain {
+	public static void main(String[] args) {
+		Waku wak = new Waku("Title bar");
+		wak.setLocation(300,200);
+		wak.setSize(400,300);
+		wak.show();
+	}
+}
---
+++ Waku.java
@@ -0,0 +1,10 @@
+package exm;
+
+import java.awt.Frame;
+
+public class Waku extends Frame {
+	public Waku(String name) {
+		setTitle( name );//Frame 
+	}
+}
+
